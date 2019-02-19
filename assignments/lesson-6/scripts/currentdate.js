9a52fd2356cb2b0773aeba3029a1864370ee5d43
bblair23 <43458627+bblair23@users.noreply.github.com>
2019-02-18 20:03:30 -0700

    Update currentdate.js

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -9,9 +9,18 @@ if(dd<10) {
 
 if(mm<10) {
     mm = '0'+mm
-    var months = ["January","February","March","April","May","June",
-    "July","August","September","October","November","December"];
 } 
+var d = new Date();
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
 
-today = months + '/' + dd + '/' + yyyy;
+  var n = weekday[d.getDay()];
+  document.write(n);
+today = mm + '/' + dd + '/' + yyyy;
 document.write(today);
