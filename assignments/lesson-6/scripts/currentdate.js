ce105119965efe1bb2a5f3b1f0a192eff68dc3a9
bblair23 <43458627+bblair23@users.noreply.github.com>
2019-02-18 20:15:11 -0700

    Update currentdate.js

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,17 +1,35 @@
 
 var today = new Date();
 var weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
+weekday[0] = "Sunday ";
+weekday[1] = "Monday ";
+weekday[2] = "Tuesday ";
+weekday[3] = "Wednesday ";
+weekday[4] = "Thursday ";
+weekday[5] = "Friday ";
+weekday[6] = "Saturday ";
 
 var n = weekday[today.getDay()];
+
 var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0!
+
+var month = new Array(12);
+weekday[0] = "January ";
+weekday[1] = "February ";
+weekday[2] = "March ";
+weekday[3] = "April ";
+weekday[4] = "May ";
+weekday[5] = "June ";
+weekday[6] = "July ";
+weekday[7] = "August ";
+weekday[8] = "September ";
+weekday[9] = "October ";
+weekday[10] = "November ";
+weekday[11] = "December ";
+
+var m = month[today.getDay()];
+
+
 var yyyy = today.getFullYear();
 
 if(dd<10) {
@@ -22,5 +40,5 @@ if(mm<10) {
     mm = '0'+mm
 } 
 
-today = n + '/' + dd + '/' + yyyy;
+today = n + m + dd + '/' + yyyy;
 document.write(today);
