f8043465b0a23b83905eab751ebc4bdeffce69ba
bblair23 <43458627+bblair23@users.noreply.github.com>
2019-03-09 22:38:31 -0700

    L9

---
+++ assignments/lesson-9/scripts/jasontown.js
@@ -0,0 +1,46 @@
+
+    var article = document.querySelector('article');
+    var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+    var townData = request.response;
+    showData(townData);
+}
+
+function showData(jsonObj) {
+  var data = jsonObj['towns'];
+      
+  for (var i = 0; i < data.length; i++) {
+      if (i == 0 || i ==  2 || i == 3|| i == 6) {
+          continue;
+        }
+    var myArticle = document.createElement('div');
+    var myH5 = document.createElement('h5');
+    var myPara1 = document.createElement('h6');
+    var myPara2 = document.createElement('p');
+    var myPara3 = document.createElement('p');
+    var myPara4 = document.createElement('p');
+    var myPhoto = document.createElement('img');
+
+    myH5.textContent = data[i].name;
+    myPara1.textContent =  data[i].motto;
+    myPara2.textContent = 'Year Founded: ' + data[i].yearfounded;
+    myPara3.textContent = 'Current Population: ' + data[i].currentPopulation;
+    myPara4.textContent = 'Average Rainfall '; + data[i].averageRainfall;
+        
+    
+    myArticle.appendChild(myH5);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myPara4);
+    myArticle.appendChild(myPhoto);
+
+    article.appendChild(myArticle);
+  }
+
+}
