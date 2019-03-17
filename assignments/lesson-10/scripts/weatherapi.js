32fef80e1f927d9d9476c6bae36596e5aee18e31
bblair23 <43458627+bblair23@users.noreply.github.com>
2019-03-16 20:21:09 -0600

    L10

---
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -0,0 +1,12 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=c5d7feb7062584451dcd1e98e37cb26f';
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
+
+   }
