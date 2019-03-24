23c9dcccc3cfacfd7732c4edd2b12e9ea8c7441f
bblair23 <43458627+bblair23@users.noreply.github.com>
2019-03-24 00:17:27 -0600

    L11

---
+++ assignments/lesson-11/scripts/jasontownpreston.js
@@ -0,0 +1,51 @@
+var article = document.querySelector('article');
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+var townData = request.response;
+homepage(townData);
+}
+
+function homepage(jsonObj) {
+var data = jsonObj['towns'];
+  
+var i = 4       
+    
+var myArticle = document.createElement('article');
+var myPara1 = document.createElement('h5');
+var myPara2 = document.createElement('h5');
+var myPara3 = document.createElement('h5');
+var myPara4 = document.createElement('h5');
+
+myPara1.textContent =  data[i].events[0];
+myPara2.textContent =  data[i].events[1];
+myPara3.textContent =  data[i].events[2];
+myPara4.textContent =  data[i].events[3];
+
+   
+myArticle.appendChild(myPara1);
+myArticle.appendChild(myPara2);
+myArticle.appendChild(myPara3);
+myArticle.appendChild(myPara4);
+
+  
+article.appendChild(myArticle);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
